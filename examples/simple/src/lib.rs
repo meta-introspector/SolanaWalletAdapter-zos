a7edd0f443e1e8db09113461077979cbacb1b10d
448 OG <superuser@448.africa>
2024-12-03 10:50:32 +0300

    Fix example errors and security advisories

--- examples/simple/src/lib.rs
+++ examples/simple/src/lib.rs
@@ -1,24 +1,5 @@
-use log::Level;
 use wallet_adapter::WalletAdapter;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn main(connect_ev_node: &str) {
-    console_error_panic_hook::set_once();
-    console_log::init_with_level(Level::Trace).unwrap();
-
-    log::info!("TARGET NODE: {}", connect_ev_node);
-
-    wasm_bindgen_futures::spawn_local(async move {
-        let mut adapter = WalletAdapter::init().unwrap();
-        adapter.connect("solflare").await.unwrap();
-
-        log::info!("CONNECTED WALLET: {:?}", &adapter.connected_wallet());
-        log::info!("CONNECTED ACCOUNT: {:?}", &adapter.connected_account());
-
-        // adapter.disconnect().await.unwrap();
-
-        let signed_msg = adapter.sign_message(b"FOO").await.unwrap();
-        log::info!("SIGNED MSG: {:?}", &signed_msg);
-    });
-}
+pub fn main(connect_ev_node: &str) {}
