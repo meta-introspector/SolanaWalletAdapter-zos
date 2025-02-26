078de849781070846212808fcb0f9ed95c42de4c
448 OG <superuser@448.africa>
2025-02-26 12:06:08 +0300

    Add better templates for yew using the new template design

---
+++ templates/yew-adapter/src/views/mod.rs
@@ -0,0 +1,23 @@
+mod dashboard;
+pub use dashboard::*;
+
+mod clusters;
+pub use clusters::*;
+
+mod accounts;
+pub use accounts::*;
+
+mod connect_first;
+pub use connect_first::*;
+
+mod send_sol;
+pub use send_sol::*;
+
+mod recieve;
+pub use recieve::*;
+
+mod airdrop;
+pub use airdrop::*;
+
+mod extras;
+pub use extras::*;
